92a5187695a47ee5beca69abf8ab394df263833a
stevenliuyi <stevenliuyi@gmail.com>
2017-02-18 12:44:43 -0500

    separate Vector and Tensor classes

---
+++ basic.hpp
@@ -0,0 +1,56 @@
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <cassert>
+#include <stdexcept>
+#include <memory>
+
+// vector
+template <typename T, unsigned Dim = 2>
+struct Vector
+{
+    Vector(): x(), y() {}
+    Vector(const T& x, const T& y): x(x), y(y) {}
+
+    T x, y;
+};
+
+// matrix
+// represent matrix in a 1D vector
+template <typename T, unsigned Dim = 2>
+class Tensor
+{
+    public:
+        // constructor 
+        Tensor(unsigned nx, unsigned ny):
+            nx_(nx), ny_(ny), data_(nx * ny, T()) {}
+
+        // assignment
+        inline void operator= (const Tensor<T, Dim>& t)
+        {
+            // need to implement size check
+            data_ = t.GetAll();
+        }
+
+        inline T& operator() (unsigned i, unsigned j)
+        {
+            // need to implement size check
+            return data_[i*ny_ + j];
+        }
+
+        inline T operator() (unsigned i, unsigned j) const
+        {
+            return data_[i*ny_ + j];
+        }
+
+        inline auto GetAll() const
+        {
+            return data_;
+        }
+
+    private:
+        std::vector<T> data_;
+        const unsigned nx_;
+        const unsigned ny_;
+};
+
--- field.hpp
+++ field.hpp
@@ -1,9 +1,4 @@
-#include <vector>
-#include <algorithm>
-#include <tuple>
-#include <cassert>
-#include <stdexcept>
-#include <memory>
+#include "basic.hpp"
 
 template <typename T, unsigned Dim>
 class Position;
@@ -20,54 +15,6 @@ class FlowField;
 template <typename T, unsigned Dim>
 class AnalyticFlowField;
 
-// vector
-template <typename T, unsigned Dim = 2>
-struct Vector
-{
-    Vector(): x(), y() {}
-    Vector(const T& x, const T& y): x(x), y(y) {}
-
-    T x, y;
-};
-
-// matrix
-// represent matrix in a 1D vector
-template <typename T, unsigned Dim = 2>
-class Tensor
-{
-    public:
-        // constructor 
-        Tensor(unsigned nx, unsigned ny):
-            nx_(nx), ny_(ny), data_(nx * ny, T()) {}
-
-        // assignment
-        inline void operator= (const Tensor<T, Dim>& t)
-        {
-            // need to implement size check
-            data_ = t.GetAll();
-        }
-
-        inline T& operator() (unsigned i, unsigned j)
-        {
-            // need to implement size check
-            return data_[i*ny_ + j];
-        }
-
-        inline T operator() (unsigned i, unsigned j) const
-        {
-            return data_[i*ny_ + j];
-        }
-
-        inline auto GetAll() const
-        {
-            return data_;
-        }
-
-    private:
-        std::vector<T> data_;
-        const unsigned nx_;
-        const unsigned ny_;
-};
 
 // flow field
 template <typename T, unsigned Dim = 2>
@@ -189,16 +136,6 @@ class Position
                 throw std::domain_error("dimensions do not match!");
 
             // fill in the values
-            //auto itx = xrange.begin();
-            //std::generate(data_.begin(), data_.end(), [&]
-            //    {
-            //        auto ity = yrange.begin();
-            //        std::vector<vec> v(ny_, vec());
-            //        std::generate(v.begin(), v.end(),
-            //            [&]{ return vec(*itx, *(ity++)); });
-            //        ++itx;
-            //        return v;
-            //    });
             for (unsigned i = 0; i < nx_; ++i)
                 for (unsigned j = 0; j < ny_; ++j)
                     data_(i,j) = vec(xrange[i], yrange[j]);
